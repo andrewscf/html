6f6acdfea21d40495d035131ce2b8357080ab679
Andrews Carlos Finato <133671760+andrewscf@users.noreply.github.com>
2023-05-27 12:17:38 -0300

    Novo curso

---
+++ Javascript/ex004/ex.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){
+    console.log('VocÊ ultrapassou a velocidade permitida')
+}
---
+++ Javascript/ex004/switch.js
@@ -0,0 +1,18 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+console.log(diaSem)
+
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+    default:
+        console.log('Deu preguiça')
+        break
+}
